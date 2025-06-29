889f3697b24c20146f6a31da66ce693045b5812a
osman976 <99673597+osman976@users.noreply.github.com>
2025-06-29 13:49:12 +0300

    Delete DOM2 directory

--- DOM2/public/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'config.php';
-// Очистка всех данных сессии и ее завершение
-$_SESSION = [];
-session_destroy();
-// Перенаправление на главную страницу
-header("Location: index.php");
-exit;
